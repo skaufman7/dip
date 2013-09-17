660266ff06f40d406187b049f06f211ad3f7a238
Steve <Steve@192.168.1.103>
2013-09-17 10:57:27 -0500

    First commit

---
+++ src/dip/lab1/student/solution1/Employee.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab1.student.solution1;
+
+/**
+ *
+ * @author Steve
+ */
+public interface Employee {
+    
+    
+    
+    
+    public abstract double getNetPay();
+    
+    
+}
---
+++ src/dip/lab1/student/solution1/Start.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab1.student.solution1;
+
+/**
+ *
+ * @author Steve
+ */
+public class Start {
+    
+}
